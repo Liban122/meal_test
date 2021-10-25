2a68b472f1d71633e8b7bc370675f35b6a00e8b7
Liban122 <botanlee515@gmail.com>
2021-10-25 15:58:42 +0200

    Y

---
+++ lib/dummy_data.dart
@@ -0,0 +1,430 @@
+import 'package:flutter/material.dart';
+
+import 'models/page_1_constructor.dart';
+import 'models/page_2_construcor.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Somalian',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Turkish',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Light & Lovely',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Indian',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Breakfast',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Asian',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c9',
+    title: 'French',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Summer',
+    color: Colors.teal,
+  ),
+];
+
+const DUMMY_MEALS = const [
+  Meal(
+    id: 'm1',
+    categories: [
+      'c1',
+    ],
+    title: 'Spaghetti with Tomato Sauce',
+    affordability: Affordability.Affordable,
+    complexity: Complexity.Simple,
+    imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg',
+    duration: 20,
+    ingredients: [
+      '4 Tomatoes',
+      '1 Tablespoon of Olive Oil',
+      '1 Onion',
+      '250g Spaghetti',
+      'Spices',
+      'Cheese (optional)'
+    ],
+    steps: [
+      'Cut the tomatoes and the onion into small pieces.',
+      'Boil some water - add salt to it once it boils.',
+      'Put the spaghetti into the boiling water - they should be done in about 10 to 12 minutes.',
+      'In the meantime, heaten up some olive oil and add the cut onion.',
+      'After 2 minutes, add the tomato pieces, salt, pepper and your other spices.',
+      'The sauce will be done once the spaghetti are.',
+      'Feel free to add some cheese on top of the finished dish.'
+    ],
+    isGlutenFree: false,
+    isVegan: true,
+    isVegetarian: true,
+    isLactoseFree: true,
+  ),
+  Meal(
+    id: 'm2',
+    categories: [
+      'c2',
+    ],
+    title: 'Rice with Beef',
+    affordability: Affordability.Pricey,
+    complexity: Complexity.Hard,
+    imageUrl:
+        'https://flavorverse.com/wp-content/uploads/2016/12/Somali-Food.jpg',
+    duration: 60,
+    ingredients: [
+      "2 tablespoons unsalted butter, or margarine",
+      "2 tablespoons vegetable oil, or olive oil",
+      "1 medium onion, grated and well drained",
+      "1 large tomato, grated, with juice",
+      "1 small green bell pepper, grated",
+      "2 cups coarse bulgur",
+      "1 teaspoon kosher salt",
+      "1/2 teaspoon freshly ground black pepper",
+      "2 tablespoons tomato paste",
+      "2 teaspoons granulated sugar",
+      "4 cups chicken broth, or water",
+      "1/2 cup coarsely chopped fresh or grilled pepper, chopped tomato, and fresh Italian parsley, for garnish"
+    ],
+    steps: [
+      "Preheat the oven to 350°F (180°C)",
+      "Make the xawaash: In a medium bowl, combine the cumin, coriander, turmeric, cardamom, black pepper, cloves, and nutmeg. Set aside.",
+      "Heat a medium oven-safe pan over medium-high heat. Add the vegetable oil, onion, tomato, garlic, and cinnamon stick. Cook until the onions have softened slightly, 5-6 minutes.",
+      "Season with salt, then add the vegetable stock cube and cilantro. Cook for 1 minute, breaking up the stock cube.",
+      "Reduce the heat to medium, then stir in the xawaash and soaked rice. Cook for about 3 minutes so rice grains lightly toast.",
+      "Pour in the hot water and increase the heat to high to bring to a boil. Cover the pot, transfer to the oven, and bake for 20 minutes. Check periodically to be sure the rice has enough water.",
+      "Remove the cooked rice from the oven and drizzle the red and orange food colorings on top. Gently mix so there are different colored grains.",
+      "Sprinkle the raisins over the hot rice, cover, and set aside.",
+      "To prepare the topping, mix the potatoes with the orange food coloring in a medium bowl (wear gloves to avoid stained fingers!).",
+      "Heat about 1 inch ( 2 cm) of oil in a large heavy-bottomed saucepan. Add the potatoes and cook for 5-6 minutes, then transfer to a baking sheet lined with paper towels to drain the excess oil.",
+      "In the same oil, fry the onion slices until browned and crispy, 2-3 minutes. Add the red peppers cooking for an additional minute. Drain on paper towels.",
+      "To assemble the rice, transfer the pilaf to a large serving platter. Top with the fried potatoes, crispy onions, and red bell peppers.",
+      "Enjoy!"
+    ],
+    isGlutenFree: false,
+    isVegan: false,
+    isVegetarian: false,
+    isLactoseFree: true,
+  ),
+  Meal(
+    id: 'm3',
+    categories: [
+      'c3',
+    ],
+    title: 'Classic Hamburger',
+    affordability: Affordability.Pricey,
+    complexity: Complexity.Simple,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2014/10/23/18/05/burger-500054_1280.jpg',
+    duration: 45,
+    ingredients: [
+      '300g Cattle Hack',
+      '1 Tomato',
+      '1 Cucumber',
+      '1 Onion',
+      'Ketchup',
+      '2 Burger Buns'
+    ],
+    steps: [
+      'Form 2 patties',
+      'Fry the patties for c. 4 minutes on each side',
+      'Quickly fry the buns for c. 1 minute on each side',
+      'Bruch buns with ketchup',
+      'Serve burger with tomato, cucumber and onion'
+    ],
+    isGlutenFree: false,
+    isVegan: false,
+    isVegetarian: false,
+    isLactoseFree: true,
+  ),
+  Meal(
+    id: 'm4',
+    categories: [
+      'c4',
+    ],
+    title: 'Bulgur and Vegetable Pilaf',
+    affordability: Affordability.Affordable,
+    complexity: Complexity.Simple,
+    imageUrl:
+        'https://www.thespruceeats.com/thmb/ZRXc8imnGAT90cIj4b9v-LYTLyg=/2396x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/bulgur-and-vegetable-pilaf-3274122-step-08-5c587d104cedfd0001efee72.jpg',
+    duration: 60,
+    ingredients: [
+      "Gather the ingredients.",
+      "Melt the butter with the oil in a covered skillet or shallow pan. Fry the drained grated onion until tender but not brown—about 5 minutes.",
+      "Add the grated tomato and juice and the grated pepper. Continue to fry until the vegetables have softened and the liquid is reduced.",
+      "Add the bulgur and stir well to combine using a wooden spoon."
+          "Add the salt, pepper, tomato paste, sugar, and broth and stir until combined. Bring the mixture to a boil, then cover and reduce the heat to low.",
+      "Let the bulgur simmer gently until all the liquid has been absorbed, about 15 to 20 minutes. Keeping the pan covered, remove it from the heat and set it aside to cool. ",
+      "Gently stir the pilaf before serving to make sure all the ingredients are well blended.",
+      "Garnish each serving with a piece of fresh or grilled pepper, tomato, and some fresh Italian parsley. Serve and enjoy!"
+          "Enjoy!"
+    ],
+    steps: [
+      'Tenderize the veal to about 2–4mm, and salt on both sides.',
+      'On a flat plate, stir the eggs briefly with a fork.',
+      'Lightly coat the cutlets in flour then dip into the egg, and finally, coat in breadcrumbs.',
+      'Heat the butter and oil in a large pan (allow the fat to get very hot) and fry the schnitzels until golden brown on both sides.',
+      'Make sure to toss the pan regularly so that the schnitzels are surrounded by oil and the crumbing becomes ‘fluffy’.',
+      'Remove, and drain on kitchen paper. Fry the parsley in the remaining oil and drain.',
+      'Place the schnitzels on awarmed plate and serve garnishedwith parsley and slices of lemon.'
+    ],
+    isGlutenFree: false,
+    isVegan: false,
+    isVegetarian: false,
+    isLactoseFree: false,
+  ),
+  Meal(
+    id: 'm5',
+    categories: [
+      'c5',
+      'c10',
+    ],
+    title: 'Salad with Smoked Salmon',
+    affordability: Affordability.Luxurious,
+    complexity: Complexity.Simple,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2016/10/25/13/29/smoked-salmon-salad-1768890_1280.jpg',
+    duration: 15,
+    ingredients: [
+      'Arugula',
+      'Lamb\'s Lettuce',
+      'Parsley',
+      'Fennel',
+      '200g Smoked Salmon',
+      'Mustard',
+      'Balsamic Vinegar',
+      'Olive Oil',
+      'Salt and Pepper'
+    ],
+    steps: [
+      'Wash and cut salad and herbs',
+      'Dice the salmon',
+      'Process mustard, vinegar and olive oil into a dessing',
+      'Prepare the salad',
+      'Add salmon cubes and dressing'
+    ],
+    isGlutenFree: true,
+    isVegan: false,
+    isVegetarian: true,
+    isLactoseFree: true,
+  ),
+  Meal(
+    id: 'm6',
+    categories: [
+      'c6',
+    ],
+    title: 'Chicken Biryani',
+    affordability: Affordability.Affordable,
+    complexity: Complexity.Hard,
+    imageUrl:
+        'https://www.teaforturmeric.com/wp-content/uploads/2020/11/Pakistani-Chicken-Biryani-2-e1608874346462.jpg',
+    duration: 240,
+    ingredients: [
+      "300g basmati rice",
+      "25g butter",
+      "1 large onion, finely sliced",
+      "1 bay leaf",
+      "3 cardamom pods",
+      "small cinnamon stick",
+      "1 tsp turmeric",
+      "4 skinless chicken breasts, cut into large chunks",
+      "4 tbsp balti curry paste",
+      "85g raisins",
+      "850ml chicken stock",
+      "30g coriander, ½ chopped, ½ leaves picked and 2 tbsp toasted flaked almonds, to serve"
+    ],
+    steps: [
+      "STEP 1"
+          "Soak 300g basmati rice in warm water, then wash in cold until the water runs clear.",
+      "STEP 2"
+          "Heat 25g butter in a saucepan and cook 1 finely sliced large onion with 1 bay leaf, 3 cardamom pods and 1 small cinnamon stick for 10 mins.",
+      "STEP 3"
+          "Sprinkle in 1 tsp turmeric, then add 4 chicken breasts, cut into large chunks, and 4 tbsp curry paste. Cook until aromatic.",
+      "STEP 4"
+          "Stir the rice into the pan with 85g raisins, then pour over 850ml chicken stock.",
+      "STEP 5"
+          "Place a tight-fitting lid on the pan and bring to a hard boil, then lower the heat to a minimum and cook the rice for another 5 mins.",
+      "STEP 6"
+          "Turn off the heat and leave for 10 mins. Stir well, mixing through 15g chopped coriander. To serve, scatter over the leaves of the remaining 15g coriander and 2 tbsp toasted almonds."
+    ],
+    isGlutenFree: true,
+    isVegan: false,
+    isVegetarian: true,
+    isLactoseFree: false,
+  ),
+  Meal(
+    id: 'm7',
+    categories: [
+      'c7',
+    ],
+    title: 'Pancakes',
+    affordability: Affordability.Affordable,
+    complexity: Complexity.Simple,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2018/07/10/21/23/pancake-3529653_1280.jpg',
+    duration: 20,
+    ingredients: [
+      '1 1/2 Cups all-purpose Flour',
+      '3 1/2 Teaspoons Baking Powder',
+      '1 Teaspoon Salt',
+      '1 Tablespoon White Sugar',
+      '1 1/4 cups Milk',
+      '1 Egg',
+      '3 Tablespoons Butter, melted',
+    ],
+    steps: [
+      'In a large bowl, sift together the flour, baking powder, salt and sugar.',
+      'Make a well in the center and pour in the milk, egg and melted butter; mix until smooth.',
+      'Heat a lightly oiled griddle or frying pan over medium high heat.',
+      'Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. Brown on both sides and serve hot.'
+    ],
+    isGlutenFree: true,
+    isVegan: false,
+    isVegetarian: true,
+    isLactoseFree: false,
+  ),
+  Meal(
+    id: 'm8',
+    categories: [
+      'c8',
+    ],
+    title: 'Creamy Indian Chicken Curry',
+    affordability: Affordability.Pricey,
+    complexity: Complexity.Challenging,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2018/06/18/16/05/indian-food-3482749_1280.jpg',
+    duration: 35,
+    ingredients: [
+      '4 Chicken Breasts',
+      '1 Onion',
+      '2 Cloves of Garlic',
+      '1 Piece of Ginger',
+      '4 Tablespoons Almonds',
+      '1 Teaspoon Cayenne Pepper',
+      '500ml Coconut Milk',
+    ],
+    steps: [
+      'Slice and fry the chicken breast',
+      'Process onion, garlic and ginger into paste and sauté everything',
+      'Add spices and stir fry',
+      'Add chicken breast + 250ml of water and cook everything for 10 minutes',
+      'Add coconut milk',
+      'Serve with rice'
+    ],
+    isGlutenFree: true,
+    isVegan: false,
+    isVegetarian: false,
+    isLactoseFree: true,
+  ),
+  Meal(
+    id: 'm9',
+    categories: [
+      'c9',
+    ],
+    title: 'Chocolate Souffle',
+    affordability: Affordability.Affordable,
+    complexity: Complexity.Hard,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2014/08/07/21/07/souffle-412785_1280.jpg',
+    duration: 45,
+    ingredients: [
+      '1 Teaspoon melted Butter',
+      '2 Tablespoons white Sugar',
+      '2 Ounces 70% dark Chocolate, broken into pieces',
+      '1 Tablespoon Butter',
+      '1 Tablespoon all-purpose Flour',
+      '4 1/3 tablespoons cold Milk',
+      '1 Pinch Salt',
+      '1 Pinch Cayenne Pepper',
+      '1 Large Egg Yolk',
+      '2 Large Egg Whites',
+      '1 Pinch Cream of Tartar',
+      '1 Tablespoon white Sugar',
+    ],
+    steps: [
+      'Preheat oven to 190°C. Line a rimmed baking sheet with parchment paper.',
+      'Brush bottom and sides of 2 ramekins lightly with 1 teaspoon melted butter; cover bottom and sides right up to the rim.',
+      'Add 1 tablespoon white sugar to ramekins. Rotate ramekins until sugar coats all surfaces.',
+      'Place chocolate pieces in a metal mixing bowl.',
+      'Place bowl over a pan of about 3 cups hot water over low heat.',
+      'Melt 1 tablespoon butter in a skillet over medium heat. Sprinkle in flour. Whisk until flour is incorporated into butter and mixture thickens.',
+      'Whisk in cold milk until mixture becomes smooth and thickens. Transfer mixture to bowl with melted chocolate.',
+      'Add salt and cayenne pepper. Mix together thoroughly. Add egg yolk and mix to combine.',
+      'Leave bowl above the hot (not simmering) water to keep chocolate warm while you whip the egg whites.',
+      'Place 2 egg whites in a mixing bowl; add cream of tartar. Whisk until mixture begins to thicken and a drizzle from the whisk stays on the surface about 1 second before disappearing into the mix.',
+      'Add 1/3 of sugar and whisk in. Whisk in a bit more sugar about 15 seconds.',
+      'whisk in the rest of the sugar. Continue whisking until mixture is about as thick as shaving cream and holds soft peaks, 3 to 5 minutes.',
+      'Transfer a little less than half of egg whites to chocolate.',
+      'Mix until egg whites are thoroughly incorporated into the chocolate.',
+      'Add the rest of the egg whites; gently fold into the chocolate with a spatula, lifting from the bottom and folding over.',
+      'Stop mixing after the egg white disappears. Divide mixture between 2 prepared ramekins. Place ramekins on prepared baking sheet.',
+      'Bake in preheated oven until scuffles are puffed and have risen above the top of the rims, 12 to 15 minutes.',
+    ],
+    isGlutenFree: true,
+    isVegan: false,
+    isVegetarian: true,
+    isLactoseFree: false,
+  ),
+  Meal(
+    id: 'm10',
+    categories: [
+      'c5',
+      'c10',
+    ],
+    title: 'Asparagus Salad with Cherry Tomatoes',
+    affordability: Affordability.Luxurious,
+    complexity: Complexity.Simple,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2018/04/09/18/26/asparagus-3304997_1280.jpg',
+    duration: 30,
+    ingredients: [
+      'White and Green Asparagus',
+      '30g Pine Nuts',
+      '300g Cherry Tomatoes',
+      'Salad',
+      'Salt, Pepper and Olive Oil'
+    ],
+    steps: [
+      'Wash, peel and cut the asparagus',
+      'Cook in salted water',
+      'Salt and pepper the asparagus',
+      'Roast the pine nuts',
+      'Halve the tomatoes',
+      'Mix with asparagus, salad and dressing',
+      'Serve with Baguette'
+    ],
+    isGlutenFree: true,
+    isVegan: true,
+    isVegetarian: true,
+    isLactoseFree: true,
+  ),
+];
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'screens/page_2_screen.dart';
+import 'screens/page_1_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            bodyText1: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            bodyText2: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            subtitle1: TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+              fontWeight: FontWeight.bold,
+            )),
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+            .copyWith(secondary: Colors.amber),
+      ),
+      // home: CategoriesScreen(),
+      initialRoute: '/', // default is '/'
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+      },
+    );
+  }
+}
